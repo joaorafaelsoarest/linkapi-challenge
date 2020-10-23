d1a58eab0744a2ab010d65b9c781cb3f8b267d8e
joaorafaelsoarest <joao.rafael@dce.ufpb.br>
2020-10-23 14:53:15 -0300

    first commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
+
+import '@shared/container';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.status).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () =>
+  // eslint-disable-next-line no-console
+  console.log('🐋 Start server!!'),
+);
